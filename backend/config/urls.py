17229c778feff4731b74674d301a852e03a9cd84
anton <antonov.anton2007vn@gmail.com>
2025-12-13 14:42:42 +0300

    feat: реализован полный CRUD для задач и восстановлена авторизация

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,12 @@
+# backend/config/urls.py
+
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-
-def task_list_view(request):
-    return JsonResponse([], safe=False)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tasks/', task_list_view),
+    path('api/', include('users.urls')),   # → /api/login/
+    path('api/', include('tasks.urls')),   # → /api/tasks/
 ]
